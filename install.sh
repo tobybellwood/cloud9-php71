c4204a1c956ec58b95e4f43d61643e7143f1544e
Toby Bellwood <toby.bellwood@finance.gov.au>
2018-01-09 14:27:10 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update -y
 
 sudo apt-get install php7.1-bz2 php7.1-curl php7.1-cli php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-json php7.1-mysql php7.1-opcache php7.1-bcmath php7.1-mbstring php7.1-soap php7.1-xml php7.1-zip -y
-sudo mv /etc/apache2/envvars /etc/apache2/envvars.bak
 sudo apt-get remove libapache2-mod-php5 -y
 sudo apt-get install libapache2-mod-php7.1 -y
-sudo cp /etc/apache2/envvars.bak /etc/apache2/envvars
 
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 
 sudo a2dismod php5
 sudo a2enmod php7.1
@@ -17,5 +15,5 @@ sudo service apache2 restart
 
 # Update MySQL to v5.6 - erases everything
 
-sudo apt-get install mysql-server-5.6 -y
+sudo apt-get install mysql-server-5.6 mysql-server-core-5.6 -y
 sudo service mysql restart
